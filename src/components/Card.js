7e2eb2f4fcf9def9daf46e3f75490cf03e2aa241
FaizanK001 <47453699+FaizanK001@users.noreply.github.com>
2021-03-26 11:03:09 +0000

    Components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,39 +1,41 @@
-import React from 'react'
-import CardItem from './CardItem'
-import '../assets/CSS/Card.css'
+import React from "react";
+import CardItem from "./CardItem";
+import "../assets/CSS/Card.css";
 
 function Card() {
-    return (
-        <div className='cards'>
-            <h1>Programming Langauges</h1>
-            <div className="cards__container">
-                <div className="cards__wrapper">
-                    <ul className="cards__items">
-                        
-                        <CardItem 
-                        src= 'images/image-html.jpg'
-                        text="Pratice with HTML"
-                        label="HTML"
-                        path="/services"/>
+  return (
+    <div className="cards">
+      <h1>Programming Langauges</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItem
+              src="images/image-html.jpg"
+              text="Pratice with HTML"
+              label="HTML"
+              path="/services"
+            />
 
-                        <CardItem 
-                        src="/images/image-script.png"
-                        text="Pratice with JavaScript "
-                        label="JavaScript"
-                        path="/services"/>
-                        
-                        <CardItem 
-                        src="/images/img-php.png"
-                        text="Pratice With PHP"
-                        label="PHP"
-                        path="/services"/>
-                        
-                        
-                     </ul>
-                </div>
-            </div>
+            <CardItem
+              src="/images/image-script.png"
+              text="Pratice with JavaScript "
+              label="JavaScript"
+              path="/services"
+            />
+
+            <CardItem
+              src="/images/img-php.png"
+              text="Pratice With PHP"
+              label="PHP"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items"></ul>
+
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
