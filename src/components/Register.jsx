83c2838eaa48990145b99c82fa3ad90a4df11611
FaizanK001 <47453699+FaizanK001@users.noreply.github.com>
2021-04-22 13:15:32 +0100

    updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from 'react'
 import '../assets/CSS/style.css'
 
-
-function Login(){
- 
-return (
-  <>
- <form action="#" class="sign-in-form">
-            <h2 class="title">Sign in</h2>
+export default function Register() {
+    return (
+        <>
+            <form action="#" class="sign-up-form">
+            <h2 class="title">Sign up</h2>
             <div class="input-field">
               <i class="fas fa-user"></i>
               <input type="text" placeholder="Username" />
             </div>
             <div class="input-field">
+              <i class="fas fa-envelope"></i>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div class="input-field">
               <i class="fas fa-lock"></i>
               <input type="password" placeholder="Password" />
             </div>
-            <input type="submit" value="Login" class="btn solid" />
-            <p class="social-text">Or Sign in with social platforms</p>
+            <input type="submit" class="btn" value="Sign up" />
+            <p class="social-text">Or Sign up with social platforms</p>
             <div class="social-media">
               <a href="#" class="social-icon">
                 <i class="fab fa-facebook-f"></i>
@@ -33,10 +35,6 @@ return (
               </a>
             </div>
           </form>
-
-
-  </>
-)
-
+        </>
+    )
 }
-export default Login;
