fa368e5c707dbafd738918267007401dd39bbf8e
FaizanK001 <47453699+FaizanK001@users.noreply.github.com>
2021-04-29 19:00:57 +0100

    Html Editor Completed

--- src/pages/Html.jsx
+++ src/pages/Html.jsx
@@ -1,39 +1,77 @@
-import React, {useState} from 'react';
-import '../App.css';
-import '../assets/CSS/Html.css'
-import Popup from '../components/Popup'
+import React, { useState } from "react";
+import "../App.css";
+import "../assets/CSS/Html.css";
+import Popup from "../components/Popup";
+import Editor from "../components/Editor";
 
-export default function Services(){
-    const [buttonPop, setButtonPopup] = useState(false);
-    return( 
-    <>  
-    <div className="Html-container">
+export default function Services() {
+  const [buttonPop, setButtonPopup] = useState(false);
+  const [html, setHtml] = useState("");
+  // const [css, setCss ] = useState('')
+
+  const srcDoc = `
+    <html>
+    <body>${html}</body>
+
+    </html>
+    
+    `;
+  return (
+    <>
+      <div className="Html-container">
         <div className="herosection-container">
-            <h1>HTML Tutorial</h1>
-            <h3><i class="fab fa-html5 fa-10x"/></h3> 
-        <div className="text">
+          <h1>HTML Tutorial</h1>
+          <h3>
+            <i class="fab fa-html5 fa-10x" />
+          </h3>
+          <div className="text">
             <h2>WHAT IS HTML ?</h2>
             <li>HTML stands for Hyper Text Markup Language </li>
-            <li>HTML is the standard markup language for creating Web pages </li>
+            <li>
+              HTML is the standard markup language for creating Web pages{" "}
+            </li>
             <li>HTML describes the structure of a Web page </li>
             <li>HTML consists of a series of elements </li>
             <li>HTML elements tell the browser how to display the content </li>
-            <li>HTML elements label pieces of content such as "this is a heading", "this is a paragraph", "this is a link", etc.</li>
-            </div>
-            
+            <li>
+              HTML elements label pieces of content such as "this is a heading",
+              "this is a paragraph", "this is a link", etc.
+            </li>
+          </div>
         </div>
-        <h1>A Simple HTML Document</h1> 
-     <div className="hpopup"> 
-          <h1>Pop up</h1>
-            <button onClick={() => setButtonPopup(true)}>Open Popup</button>
+        <h1>A Simple HTML Document</h1>
+
+        <div className="card-temple">
+        <div className="hpopup">
+          <button onClick={() => setButtonPopup(true)}>Open Popup</button>
+        </div>
+
+
+        </div>
+
+        <div className="popup-text">
+          <Popup trigger={buttonPop} setTrigger={setButtonPopup}>
+            <div className="pane">
+              <Editor
+                language="xml"
+                displayName="HTML"
+                value={html}
+                onChange={setHtml}
+              />
             </div>
-            <div className="popup-text">
-            <Popup trigger={buttonPop} setTrigger={setButtonPopup}>
-                <h3>My popup</h3>
-                <p>This is my button triggered popup</p>
-            </Popup>
+            <div className="pane">
+              <iframe
+                srcDoc={srcDoc}
+                title="output"
+                sandbox="allow-scripts"
+                frameborder="0"
+                width="50%"
+                height="50%"
+              />
             </div>
-            </div>
+          </Popup>
+        </div>
+      </div>
     </>
-    )
+  );
 }
