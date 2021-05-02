6954f0e3697821ac2a6c7d65fcc2f81de64be04b
FaizanK001 <47453699+FaizanK001@users.noreply.github.com>
2021-05-02 18:17:04 +0100

    Login and registration completed

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -11,24 +11,25 @@ export default function Signin() {
    // const [loginFormstatus, setLoginFormtatus] = useState ();
 
    const loginProps = useSpring({
-       left : registrationFormStatus ? -430: 0,
+       left : registrationFormStatus ? -500: 0,
        opacity: registrationFormStatus ? 0 : 1
    })
    const registerProps = useSpring({
-    left : registrationFormStatus ? 0 : 430,
+    left : registrationFormStatus ? 0 : 500,
     opacity: registrationFormStatus ? 1 : 0
 })
 
-    function registerClicked(){alert('Register clicked'); setRegisterFormStatus(true)}
-    function loginClicked(){alert('Login clicked'); setRegisterFormStatus(false)}
+
+    function registerClicked(){ setRegisterFormStatus(true)}
+    function loginClicked(){ setRegisterFormStatus(false)}
 
     return (
         
          <div className="container">
              <div className="login-register-wrapper">
                  <div className="nav-buttons">
-                     <button onClick={loginClicked}   id="loginBtn" className='active'>Login</button>
-                     <button onClick={registerClicked}    id="registerBtn" className=' '>Register</button>
+                     <label onClick={loginClicked}   id="loginBtn" className='active'>Login</label>
+                     <label onClick={registerClicked}    id="registerBtn" className=' '>Register</label>
                  </div>
                  <div className="form-group">
                     <animated.form action='' id='loginform' style={loginProps}><LoginForm/></animated.form> 
