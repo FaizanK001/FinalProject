b8312991122edd16ccb32836ec14633c911126bf
FaizanK001 <47453699+FaizanK001@users.noreply.github.com>
2021-05-07 10:06:33 +0100

    updated

--- src/components/CssHeroSection.jsx
+++ src/components/CssHeroSection.jsx
@@ -9,19 +9,21 @@ export default function CssHeroSection() {
                 <h3>
                 <i className="fab fa-css3-alt fa-10x"></i>
           </h3>
+          
             <div className="css-text-container">
-            <h2>WHAT IS HTML ?</h2>
-            <li>HTML stands for Hyper Text Markup Language </li>
-            <li>
-              HTML is the standard markup language for creating Web pages{" "}
-            </li>
-            <li>HTML describes the structure of a Web page </li>
-            <li>HTML consists of a series of elements </li>
-            <li>HTML elements tell the browser how to display the content </li>
-            <li>
-              HTML elements label pieces of content such as "this is a heading",
-              "this is a paragraph", "this is a link", etc.
-            </li>
+            <h2>WHAT IS CSS ?</h2>
+            <li>CSS is the language we use to style an HTML document. </li>
+          <li>CSS describes how HTML elements should be displayed. </li>
+          <li>This tutorial will teach you CSS from basic to advanced.</li>
+          <li>CSS stands for Cascading Style Sheets</li>
+          <li>CSS describes how HTML elements are to be displayed on screen, paper, or in other media </li>
+          <li>
+          CSS saves a lot of work. It can control the layout of multiple web pages all at once
+          </li>
+          <li>
+          External stylesheets are stored in CSS files
+          </li>
+            <p></p>
             </div>
 
             </div>
--- src/components/JsHeroSection.jsx
+++ src/components/JsHeroSection.jsx
@@ -10,18 +10,15 @@ export default function JsHeroSection() {
             <i className="fab fa-js-square fa-10x"></i>
           </h3>
           <div className="js-text">
-          <h2>WHAT IS HTML ?</h2>
-            <li>HTML stands for Hyper Text Markup Language </li>
+          <h2>WHAT IS JavaScript ?</h2>
+            <li>JavaScript is the world's most popular programming language. </li>
             <li>
-              HTML is the standard markup language for creating Web pages{" "}
+            JavaScript is the programming language of the Web.
             </li>
-            <li>HTML describes the structure of a Web page </li>
-            <li>HTML consists of a series of elements </li>
-            <li>HTML elements tell the browser how to display the content </li>
-            <li>
-              HTML elements label pieces of content such as "this is a heading",
-              "this is a paragraph", "this is a link", etc.
-            </li>
+            <li>JavaScript is easy to learn. </li>
+            <li>This tutorial will teach you JavaScript from basic to advanced.</li>
+            <li>JavaScript is a cross-platform, object-oriented scripting language used to make webpages interactive </li>
+    
           </div>
         </div>
       </div>
